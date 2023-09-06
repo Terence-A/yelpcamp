554900991c7b83d644ce2c9c49f13a891b035f7e
terenceDev <terence.arseneault@hotmail.com>
2023-09-06 20:14:17 -0300

    added image seeds and set up to show images and descriptions on show page

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://unsplash.com/collections/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, aut tempora! Voluptatibus, voluptate illo, dolores, sit omnis delectus numquam quisquam dolorum ex quas enim? Eius, exercitationem perspiciatis. Necessitatibus, alias dolor.",
+      price,
     });
     await camp.save();
   }
