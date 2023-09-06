6aef04fcb3b22f40c7b7eba777ef83dd816e4308
terenceDev <terence.arseneault@hotmail.com>
2023-09-06 07:01:15 -0300

    added basic show endpoint showing title and location without routes

--- server.js
+++ server.js
@@ -27,13 +27,14 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 //campground
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard",
-    description: "Cheap Camping",
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find();
+  res.render("campgrounds/index", { campgrounds });
+});
+//single campground
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 //Server listen
